daf41483d81f9e08c453d5eb3ae4e6828d30fb21
Rabi MARHOUM <rabi.marhoum@outlook.com>
2025-08-27 23:09:23 -0400

    Section3 Villa Model

---
+++ WhiteLagoon.Web/Controllers/VillaController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WhiteLagoon.Infrastructure.Data;
+using WitheLagoon.Domain.Entities;
+
+namespace WhiteLagoon.Web.Controllers
+{
+	public class VillaController : Controller
+	{
+		private readonly ApplicationDbContext _context;
+		public VillaController(ApplicationDbContext context)
+		{
+			_context = context;
+		}
+		public IActionResult Index()
+		{
+			var villas = _context.Villas.ToList();
+			return View(villas);
+		}
+
+		public IActionResult Create()
+		{
+			return View();
+		}
+		[HttpPost]
+		public IActionResult Create(Villa obj)
+		{
+			if (obj.Name == obj.Description)
+			{
+				ModelState.AddModelError("name", "The description cannot exactly match the Name.");
+			}
+			if (ModelState.IsValid)
+			{
+				_context.Villas.Add(obj);
+				_context.SaveChanges();
+				return RedirectToAction("Index");
+			}
+			return View();
+		}
+
+	}
+}
---
+++ WitheLagoon.Domain/Entities/Villa.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WitheLagoon.Domain.Entities
+{
+	public class Villa
+	{
+		public int Id { get; set; }
+		[MaxLength(50, ErrorMessage = "Name cannot exceed 50 characters")]
+		public required string Name { get; set; }
+		public string? Description { get; set; }
+		[Display(Name = "Price per night")]
+		[Range(1, 10000, ErrorMessage = "Price must be between 1 and 10,000")]
+		public double Price { get; set; }
+		public int Sqft { get; set; }
+		[Range(1, 20, ErrorMessage = "Occupancy must be between 1 and 20")]
+		public int Occupancy { get; set; }
+		[Display(Name = "Image Url")]
+		public string? ImageUrl { get; set; }
+		public DateTime? CreatedDate { get; set; }
+		public DateTime? UpdatedDate { get; set; }
+
+
+
+	}
+}
