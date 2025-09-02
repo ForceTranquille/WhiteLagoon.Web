2a335eb0e7dba61559099e161c431b227e8b9db0
Rabi MARHOUM <rabi.marhoum@outlook.com>
2025-09-01 23:25:36 -0400

    Section 8 : Images and Villa

--- WhiteLagoon.Web/Controllers/VillaController.cs
+++ WhiteLagoon.Web/Controllers/VillaController.cs
@@ -8,9 +8,11 @@ namespace WhiteLagoon.Web.Controllers
 	public class VillaController : Controller
 	{
 		private readonly IUnitOfWork _unitOfWork;
-		public VillaController(IUnitOfWork unitOfWork)
+		private readonly IWebHostEnvironment _webHostEnvironment;
+		public VillaController(IUnitOfWork unitOfWork, IWebHostEnvironment webHostEnvironment)
 		{
 			_unitOfWork = unitOfWork;
+			_webHostEnvironment = webHostEnvironment;
 		}
 		public IActionResult Index()
 		{
@@ -31,6 +33,21 @@ namespace WhiteLagoon.Web.Controllers
 			}
 			if (ModelState.IsValid)
 			{
+				if (obj.Image != null)
+				{
+					string fileName = Guid.NewGuid().ToString() + Path.GetExtension(obj.Image.FileName);
+					string imagePath = Path.Combine(_webHostEnvironment.WebRootPath, @"images\VillaImages");
+					using var fileStream = new FileStream(Path.Combine(imagePath, fileName), FileMode.Create);
+					obj.Image.CopyTo(fileStream);
+					obj.ImageUrl = @"\images\VillaImages\" + fileName;
+
+				}
+				else
+				{
+					obj.ImageUrl = "https://placehold.co/600x400";
+
+
+				}
 				_unitOfWork.Villa.Add(obj);
 				_unitOfWork.Save();
 				TempData["success"] = "The villa has been created successfully.";
@@ -53,6 +70,27 @@ namespace WhiteLagoon.Web.Controllers
 		{
 			if (ModelState.IsValid && obj.Id > 0)
 			{
+				if (obj.Image != null)
+				{
+					string fileName = Guid.NewGuid().ToString() + Path.GetExtension(obj.Image.FileName);
+					string imagePath = Path.Combine(_webHostEnvironment.WebRootPath, @"images\VillaImages");
+
+					if (!string.IsNullOrEmpty(obj.ImageUrl))
+					{
+						var oldImagePath = Path.Combine(_webHostEnvironment.WebRootPath, obj.ImageUrl.TrimStart('\\'));
+
+						if (System.IO.File.Exists(oldImagePath))
+						{
+							System.IO.File.Delete(oldImagePath);
+						}
+					}
+					
+					
+					using var fileStream = new FileStream(Path.Combine(imagePath, fileName), FileMode.Create);
+					obj.Image.CopyTo(fileStream);
+					obj.ImageUrl = @"\images\VillaImages\" + fileName;
+				}
+				
 				_unitOfWork.Villa.Update(obj);
 				_unitOfWork.Save();
 				TempData["success"] = "The villa has been updated successfully.";
@@ -77,6 +115,16 @@ namespace WhiteLagoon.Web.Controllers
 			Villa? objFromDb = _unitOfWork.Villa.Get(v => v.Id == obj.Id);
 			if (objFromDb is not null)
 			{
+
+				if (!string.IsNullOrEmpty(objFromDb.ImageUrl))
+				{
+					var oldImagePath = Path.Combine(_webHostEnvironment.WebRootPath, objFromDb.ImageUrl.TrimStart('\\'));
+
+					if (System.IO.File.Exists(oldImagePath))
+					{
+						System.IO.File.Delete(oldImagePath);
+					}
+				}
 				_unitOfWork.Villa.Remove(objFromDb);
 				_unitOfWork.Save();
 				TempData["success"] = "The villa has been deleted successfully.";
--- WitheLagoon.Domain/Entities/Villa.cs
+++ WitheLagoon.Domain/Entities/Villa.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,6 +21,8 @@ namespace WitheLagoon.Domain.Entities
 		public int Sqft { get; set; }
 		[Range(1, 20, ErrorMessage = "Occupancy must be between 1 and 20")]
 		public int Occupancy { get; set; }
+		[NotMapped]
+		public IFormFile? Image { get; set; }
 		[Display(Name = "Image Url")]
 		public string? ImageUrl { get; set; }
 		public DateTime? CreatedDate { get; set; }
